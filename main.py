7148ed3a4ff91b711ad9ef8afd2a3fdfdae404ae
maxFZ <maksym2234@gmail.com>
2021-01-18 20:52:11 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from tkinter import *
+import pandas
+import random
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient='records')
+
+
+def next_card():
+    curr_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text='French')
+    canvas.itemconfig(card_word, text=curr_card['French'])
+
+
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title('Flashy')
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526)
+card_font_img = PhotoImage(file='images/card_front.png')
+canvas.create_image(400, 263, image=card_font_img)
+card_title = canvas.create_text(400, 150, text='', font=('Arial', 40, 'italic'))
+card_word = canvas.create_text(400, 263, text='', font=('Arial', 60, 'bold'))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+#Buttons
+cross_img = PhotoImage(file='images/wrong.png')
+unknown_btn = Button(image=cross_img, highlightthickness=0, command=next_card)
+unknown_btn.grid(row=1, column=0)
+
+check_img = PhotoImage(file='images/right.png')
+known_btn = Button(image=check_img, highlightthickness=0, command=next_card)
+known_btn.grid(row=1, column=1)
+
+next_card()
+
+window.mainloop()
